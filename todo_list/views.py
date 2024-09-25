2182b05bb8d5f8a393d19c81ed4e85a664cb1994
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-25 13:15:35 +0300

    add to views - task create, completion toggle, update, delete refactor list view

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,44 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 
-from todo_list.models import Task
+from todo_list.forms import TaskCreateUpdateForm
+from todo_list.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
-    template_name = "todo_list/task_list.html"
+    paginate_by = 5
+    queryset = Task.objects.prefetch_related(
+        "tags"
+    ).order_by("is_done", "-created_at")
+
+
+class TaskCompletionToggleView(generic.View):
+    def post(self, request: HttpRequest, pk: int) -> HttpResponse:
+        task = get_object_or_404(Task, pk=pk)
+        is_complete = request.POST.get("is_complete")
+        if is_complete == "done":
+            task.is_done = True
+        elif is_complete == "undo":
+            task.is_done = False
+        task.save()
+        return redirect(reverse("todo_list:task-list"))
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskCreateUpdateForm
+    success_url = reverse_lazy("todo_list:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskCreateUpdateForm
+    success_url = reverse_lazy("todo_list:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo_list:task-list")

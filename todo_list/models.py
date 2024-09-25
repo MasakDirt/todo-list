9995f4aa60c78af50019d44cf286b02ee165e552
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-25 10:48:09 +0300

    create models for todo_list and make migrations

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ("-deadline", )
+
+    def __str__(self) -> str:
+        return f"{self.content} - {self.deadline}"

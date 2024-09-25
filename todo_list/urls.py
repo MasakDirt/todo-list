636c829c12e42b0042233b8a9220e3482f95f256
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-25 13:16:05 +0300

    add all created view to urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,10 +1,36 @@
 from django.urls import path
 
-from todo_list.views import TaskListView
+from todo_list.views import (
+    TaskListView,
+    TaskCompletionToggleView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+)
 
 
 app_name = "todo_list"
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list")
+    path("", TaskListView.as_view(), name="task-list"),
+    path(
+        "tasks/create",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path(
+        "tasks/<int:pk>/toggle/",
+        TaskCompletionToggleView.as_view(),
+        name="task-toggle"
+    ),
 ]
